745dc2b3408187920b69441770b942e132629667
Elliott Lynch <elynch90@gmail.com>
2023-03-07 00:33:19 -0800

    Update image_jam.py

--- src/image_jam.py
+++ src/image_jam.py
@@ -1,4 +1,4 @@
-# script for edditing the images
+# include dependencies
 from os import path, getcwd
 import numpy as np
 from PIL import Image
@@ -13,18 +13,23 @@ test_img = Image.open(fp)
 test_img_array = np.asarray(test_img)
 print(test_img_array.shape)
 
+
 # colorize the image
-
-
 def set_color(image_path, r_val, g_val, b_val, alpha, invert_flag):
+    """Set the color of the image"""
     cur_image = Image.open(str(image_path))
     image_tensor = np.asarray(cur_image)
     # set red values
-    r_new = image_tensor[:, :, 0] * int(r_val / 100)
+    r_new = image_tensor[:, :, 0] * int(r_val / 100)  
     # set green values
     g_new = image_tensor[:, :, 1] * int(g_val / 100)
     # set blue values
     b_new = image_tensor[:, :, 2] * int(b_val / 100)
+
+    # clamp the values to 0-255
+    r_new = np.clip(r_new, 0, 255)
+    g_new = np.clip(g_new, 0, 255)
+    b_new = np.clip(b_new, 0, 255)
     # create an updated image tensor based on 100 new color arrays
     update_array = np.zeros((image_tensor.shape), dtype=np.uint8)
     update_array[:, :, 0] = r_new  # set red channel
@@ -44,6 +49,7 @@ def set_color(image_path, r_val, g_val, b_val, alpha, invert_flag):
 
 
 def set_alpha(image_tensor, alpha):
+    """Set the alpha of the image"""
     # set red values
     image_tensor[:, :, 0] *= alpha
     return image_tensor
@@ -51,6 +57,7 @@ def set_alpha(image_tensor, alpha):
 
 def image_subupdate(cur_filepath, r_val, g_val, b_val,
                     alpha, img_w, img_h, invert_flag, window):
+    """Update the image"""
     # pass the current slider values to the colorizer
     image_update = set_color(cur_filepath, r_val, g_val,
                              b_val, alpha, invert_flag)
@@ -61,12 +68,14 @@ def image_subupdate(cur_filepath, r_val, g_val, b_val,
 
 
 def save_img(cur_filepath, r_val, g_val, b_val, alpha, invert_flag, window):
+    """Save the image to the given filepath"""
     file_path = str(tk.filedialog.asksaveasfilename()) + ".png"
     cur_image = set_color(cur_filepath, r_val, g_val,
                           b_val, alpha, invert_flag)
     # save the image to the given filepath
     cur_image.save(file_path, format="png")
-    window.Element("SAVEDIR").update(Text=file_path)
+    # update the save directory text
+    window.Element("SAVEDIR").update(file_path)
 
 
 def upload_img(cur_filepath, r_val, g_val, b_val, alpha,
